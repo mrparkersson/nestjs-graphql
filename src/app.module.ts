1e4325d335b229f525a0afdd5700203d67364d44
mrparkersson <amoparkerprince78@gmail.com>
2022-08-13 15:50:10 +0000

    Add autoSchemafile

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,13 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { UsersModule } from './users/users.module';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      autoSchemaFile: true,
+      autoSchemaFile: join(process.cwd(), 'src/users'),
     }),
     UsersModule,
   ],
