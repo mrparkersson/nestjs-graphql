f2c652d1c12cc00c1b7d9bf9e8266c340ebba965
mrparkersson <amoparkerprince78@gmail.com>
2022-08-13 16:13:59 +0000

    Add input type to deleteuser

--- src/users/dto/inputs/delete-user-input.ts
+++ src/users/dto/inputs/delete-user-input.ts
@@ -1,7 +1,7 @@
-import { Field, ObjectType } from '@nestjs/graphql';
+import { Field, InputType } from '@nestjs/graphql';
 import { IsNotEmpty } from 'class-validator';
 
-@ObjectType()
+@InputType()
 export class DeleteUser {
   @Field()
   @IsNotEmpty()
